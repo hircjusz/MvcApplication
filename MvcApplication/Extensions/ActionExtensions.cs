3ae65d497036759835a9cef14b46e9b6236a4442
unknown <dauh@SWMINDDAUH2.ailleron.lan>
2016-09-16 11:32:05 +0200

    commit start

---
+++ MvcApplication/Extensions/ActionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Extensions
+{
+    public static class ActionExtensions
+    {
+
+        public static bool ActionAuthorized(this HtmlHelper htmlHelper, string action, string controller)
+        {
+            ControllerBase controllerBase = htmlHelper.ViewContext.Controller;
+            var controllerContext = new ControllerContext(htmlHelper.ViewContext.RequestContext, controllerBase);
+
+            var controllerDescriptor = new ReflectedControllerDescriptor(controllerContext.Controller.GetType());
+            var actionDescriptor = controllerDescriptor.FindAction(controllerContext, action);
+
+            if (actionDescriptor == null)
+            {
+                return false;
+            }
+
+            //FilterInfo filters= new FilterInfo(FilterProviders);
+
+            return true;
+
+        }
+
+    }
+}
---
+++ MvcApplication/Extensions/LinkExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcApplication.Extensions
+{
+    public static class LinkExtensions
+    {
+
+        public static MvcHtmlString ActionLinkAuthorized(this HtmlHelper htmlHelper, string linkText, string actionName,
+            string controllerName, RouteValueDictionary routeValues=null, IDictionary<string, object> htmlAttributes=null, bool showActionLinkAsDisabled=false)
+        {
+
+            if (htmlHelper.ActionAuthorized(actionName, controllerName))
+            {
+                return MvcHtmlString.Create("Action Authorized");
+            }
+
+            return MvcHtmlString.Create("Hello Darek");
+        }
+
+    }
+}
